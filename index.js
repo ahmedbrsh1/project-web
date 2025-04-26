64aae7e6ee445369ded25b63fd284c285386fab9
mohamed ahmed tarek <145493369+M-2004-lap@users.noreply.github.com>
2025-04-26 13:32:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
  const  container=document.querySelector('.container');
  const  LoginLink=document.querySelector('.signInLink')
  const RegisterLink=document.querySelector('.signUpLink')
+ const loginError = document.getElementById('error');
+ const registerError = document.getElementById('error2');
 RegisterLink.addEventListener('click',()=>{
     container.classList.add('active');
+    loginError.innerText = ''
 })
 LoginLink.addEventListener('click',()=>{
     container.classList.remove('active');
+    registerError.innerText = ''
+
 })
